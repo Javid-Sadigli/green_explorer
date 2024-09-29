543dd1960c649bc3ed6c2a8f71fd0cfda713bb6b
Javid Sadigli <cavidoffical@gmail.com>
2024-09-29 16:14:57 +0400

    Database URI added

--- data/database.js
+++ data/database.js
@@ -6,7 +6,7 @@ let _db;
 
 // Connect the database
 const mongoConnect = (CALLBACK_FUNCTION) => {
-    MongoClient.connect('YOUR_MONGODB_DATABASE_PATH').then((client) => {
+    MongoClient.connect('mongodb+srv://javidsadiglimongodbclusters:yo2y7F5hixWihDwM@cluster0.1cske.mongodb.net/green_explorer?retryWrites=true&w=majority&appName=Cluster0').then((client) => {
         _db = client.db();
         CALLBACK_FUNCTION(client);
     }).catch((err) => {
